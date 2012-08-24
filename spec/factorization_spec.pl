71541b860064b497514c88628a3ed5176ec7c40f
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-08-24 01:08:14 -0500

    rewrite

--- spec/factorization_spec.pl
+++ spec/factorization_spec.pl
@@ -4,38 +4,25 @@
 :- begin_tests(factorization).
 
 test(1) :-
-	assertion(factorization:factor([],1)).
+	assertion(factor([], 1)).
 
 test(2) :-
-	assertion(factorization:factor([2],2)).
+	assertion(factor([2], 2)).
 
 test(3) :-
-	assertion(factorization:factor([3],3)).
+	assertion(factor([3], 3)).
 
 test(4) :-
-	assertion(factorization:factor([2, 2], 4)).
+	assertion(factor([2,2], 4)).
 
-test(5) :-
-	assertion(factorization:factor([5], 5)).
-
-test(6) :-
-	assertion(factorization:factor([2, 3], 6)).
+test(only_ascending) :-
+	assertion(\+factor([3,2], 6)).
 
 test(no_composites_allowed) :-
-	assertion(\+factorization:factor([9],9)).
+	assertion(\+factor([9], 9)).
 
-test(ascending_order) :-
-	assertion(\+factorization:factor([3,2],6)).
-
-test(finds_answer) :-
-	factorization:factor(Numbers, 6),
-	Numbers = [2,3].
-
-test(finds_hard_answer) :-
-	factorization:factor(Numbers, 420),
-	Numbers = [2,2,3,5,7].
-
-test(30) :-
-	assertion(factorization:factor([2, 3, 5], 30)).
+test(gets_the_answer) :-
+	factor(Factors, 420), !,
+	assertion(Factors = [2,2,3,5,7]).
 
 :- end_tests(factorization).
--- src/factorization.pl
+++ src/factorization.pl
@@ -1,30 +1,25 @@
 :-  module(factorization, [factor/2]).
-:- use_module(library(clpfd)).
+:-  use_module(library(clpfd)).
 
-primes_multiply_to(Numbers, 1) :-
-	Numbers=[].
-
-primes_multiply_to(Numbers, Product) :-
-	Numbers = [First|Rest],
-	First * Quotient #= Product,
-	prime(First),
-	primes_multiply_to(Rest, Quotient).
-
-factor(Numbers, Product) :-
-	primes_multiply_to(Numbers, Product),
-	sorted(Numbers).
-
-sorted(Numbers) :-
-	msort(Numbers, Numbers).
-
-prime(Number) :-
-	Dec + 1 #= Number,
-	prime(Number, Dec).
-
-prime(_, 1) :-
+factor([], 1) :-
 	true.
 
-prime(Number, Divisor) :-
-  Number mod Divisor #\= 0,
-	Dec + 1 #= Divisor,
-	prime(Number, Dec).
+factor(Factors, Product) :-
+	[First|Rest] = Factors,
+	First * Quotient #= Product,
+	prime(First),
+	factor(Rest, Quotient),
+	msort(Factors, Factors).
+
+prime(Number) :-
+	Decremented #= Number - 1,
+	has_no_divisors_below(Number, Decremented).
+
+has_no_divisors_below(_, 1) :-
+	true.
+
+has_no_divisors_below(Number, Divisor) :-
+	Remainder #= Number mod Divisor,
+	Remainder #\= 0,
+	Decremented #= Divisor - 1,
+	has_no_divisors_below(Number, Decremented).
