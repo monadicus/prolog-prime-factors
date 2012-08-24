12d01bb3252d2ad3a332fb2664973b9494e0d77b
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-08-23 22:57:53 -0500

    WIN

--- spec/factorization_spec.pl
+++ spec/factorization_spec.pl
@@ -14,10 +14,27 @@ test(3) :-
 
 test(4) :-
 	assertion(factorization:factor([2, 2], 4)).
+
 test(5) :-
 	assertion(factorization:factor([5], 5)).
+
 test(6) :-
 	assertion(factorization:factor([2, 3], 6)).
+
+test(no_composites_allowed) :-
+	assertion(\+factorization:factor([9],9)).
+
+test(ascending_order) :-
+	assertion(\+factorization:factor([3,2],6)).
+
+test(finds_answer) :-
+	factorization:factor(Numbers, 6),
+	Numbers = [2,3].
+
+test(finds_hard_answer) :-
+	factorization:factor(Numbers, 420),
+	Numbers = [2,2,3,5,7].
+
 test(30) :-
 	assertion(factorization:factor([2, 3, 5], 30)).
 
--- src/factorization.pl
+++ src/factorization.pl
@@ -1,18 +1,30 @@
 :-  module(factorization, [factor/2]).
+:- use_module(library(clpfd)).
 
-factor(Numbers, 1) :-
+primes_multiply_to(Numbers, 1) :-
 	Numbers=[].
 
-factor(Numbers, 4) :-
-	Numbers=[2,2].
+primes_multiply_to(Numbers, Product) :-
+	Numbers = [First|Rest],
+	First * Quotient #= Product,
+	prime(First),
+	primes_multiply_to(Rest, Quotient).
 
 factor(Numbers, Product) :-
-	Numbers=[Product].
+	primes_multiply_to(Numbers, Product),
+	sorted(Numbers).
 
-factor(Numbers, Product) :-
-	Numbers = [Smallest|Rest],
-	Quotient is Product / Smallest,
-	write(Smallest),
-	write(Quotient),
-	factor(Rest, Quotient).
+sorted(Numbers) :-
+	msort(Numbers, Numbers).
 
+prime(Number) :-
+	Dec + 1 #= Number,
+	prime(Number, Dec).
+
+prime(_, 1) :-
+	true.
+
+prime(Number, Divisor) :-
+  Number mod Divisor #\= 0,
+	Dec + 1 #= Divisor,
+	prime(Number, Dec).
